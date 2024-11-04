ace291d8f439d30ef8d862cfc6d51ca04ef73225
ICTDEV OJT <ictdev@princeretail.com>
2024-11-04 10:01:52 +0800

    New Udpated - clark

--- NatalCare.Models/ViewModel/Patient/PatientsVM.cs
+++ NatalCare.Models/ViewModel/Patient/PatientsVM.cs
@@ -7,7 +7,9 @@ namespace NatalCare.Models.ViewModel.Patient
     {
         public Patients? Patient { get; set; }              // Single patient record
         public List<Patients>? PatientList { get; set; }    // List of patients
-        public Invoice? PrenatalRecord { get; set; }       // Single prenatal records
+        public Prenatal? PrenatalRecord { get; set; }       // Single prenatal records
         public List<Prenatal>? PrenatalRecords { get; set; }   // List of prenatal records
+        public PrenatalVisit? PrenatalVisitRecord { get; set; }   // Single prenatal visit records
+        public List<PrenatalVisit>? PrenatalVisitRecords { get; set; } // List of prenatal visit records 
     }
 }
